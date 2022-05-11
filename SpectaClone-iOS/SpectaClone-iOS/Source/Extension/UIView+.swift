91bb8a1f70d4dbb4c7ff11a07aca93fb43b08839
hyun99999 <ypsophila97@icloud.com>
2022-05-12 00:46:03 +0900

    add: UIView extension 추가
    - addSubviews: 여러개의 UIView 를 한번에 addSubview

---
+++ SpectaClone-iOS/SpectaClone-iOS/Source/Extension/UIView+.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+.swift
+//  SpectaClone-iOS
+//
+//  Created by kimhyungyu on 2022/05/11.
+//
+
+import UIKit
+
+extension UIView {
+    /// 여러개의 UIView 를 한번에 addSubview 해준다.
+    func addSubviews(_ views: [UIView]) {
+        views.forEach { self.addSubview($0) }
+    }
+}
