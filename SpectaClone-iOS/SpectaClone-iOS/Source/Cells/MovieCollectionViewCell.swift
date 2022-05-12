e58f9f84c79168d2be985d8771012c6508f39b87
hyun99999 <ypsophila97@icloud.com>
2022-05-12 15:24:04 +0900

    feat: `MovieCollectionViewCell` 셀의 레이아웃 및 초기화 메서드 생성

--- SpectaClone-iOS/SpectaClone-iOS/Source/Cells/MovieCollectionViewCell.swift
+++ SpectaClone-iOS/SpectaClone-iOS/Source/Cells/MovieCollectionViewCell.swift
@@ -13,6 +13,15 @@ class MovieCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let posterImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.image = UIImage(systemName: "circle")
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.textColor = .white
+        $0.font = .boldSystemFont(ofSize: 16)
+    }
     
     // MARK: - View Life Cycles
     
@@ -20,6 +29,7 @@ class MovieCollectionViewCell: UICollectionViewCell {
         super.init(frame: frame)
         
         setUI()
+        setLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -35,6 +45,37 @@ class MovieCollectionViewCell: UICollectionViewCell {
 
 extension MovieCollectionViewCell {
     private func setUI() {
+        self.backgroundColor = .black
+    }
+    
+    func initCellWith(url: String, title: String) {
+        if let url = URL(string: Const.URL.imageUrl + url) {
+            // TODO: - 이미지 다운로드.
+        }
         
+        titleLabel.text = title
+    }
+}
+
+// MARK: - Layout
+
+extension MovieCollectionViewCell {
+    private func setLayout() {
+        self.addSubviews([posterImageView, titleLabel])
+    
+        guard let superview = self.superview else { return }
+        let posterImageViewConstraints: [NSLayoutConstraint] = [
+            posterImageView.leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            posterImageView.trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            posterImageView.topAnchor.constraint(equalTo: superview.topAnchor),
+            posterImageView.heightAnchor.constraint(equalTo: posterImageView.widthAnchor, multiplier: 1.0)
+        ]
+        NSLayoutConstraint.activate(posterImageViewConstraints)
+        
+        let titleLabelConstraints: [NSLayoutConstraint] = [
+            titleLabel.leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 10.0)]
+        NSLayoutConstraint.activate(titleLabelConstraints)
     }
 }
