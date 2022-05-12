e3a234265d66ea6335ce97447bfbf20059235b1b
hyun99999 <ypsophila97@icloud.com>
2022-05-12 14:43:21 +0900

    add: error 핸들링 하기 위한 Error 을 채택한 열거형 생성

---
+++ SpectaClone-iOS/SpectaClone-iOS/Resource/Models/MovieError.swift
@@ -0,0 +1,13 @@
+//
+//  MovieError.swift
+//  SpectaClone-iOS
+//
+//  Created by kimhyungyu on 2022/05/12.
+//
+
+import Foundation
+
+enum MovieError: Error {
+    case invalidServerResponse
+    case unsupportImage
+}
