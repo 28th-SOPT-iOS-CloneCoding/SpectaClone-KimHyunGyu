ec5ab52b0a5c10b9ef1bfb355da0fcd2fd25477d
hyun99999 <ypsophila97@icloud.com>
2022-05-26 02:23:28 +0900

    feat: NetworkProvider 생성
    - TargetType 을 기반으로 한 Switch 문을 사용해서 여러가지 서버통신을 위한 request 생성

---
+++ SpectaClone-iOS/SpectaClone-iOS/Source/Module/NetworkProvider.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkProvider.swift
+//  SpectaClone-iOS
+//
+//  Created by kimhyungyu on 2022/05/20.
+//
+
+import Foundation
+
+struct NetworkProvider<Target: TargetType> {
+    func request(_ target: Target) throws -> URLRequest {
+        
+        // url path
+        let path = target.baseURLPath + target.path
+        guard var urlComponents = URLComponents(string: path) else {
+            throw MovieDownloadError.invalidURLString
+        }
+        
+        // task
+        var url: URL?
+        let task = target.task
+        switch task {
+        case .reqiestPlan:
+            url = urlComponents.url
+        case .requestParameters(let parameters, let encoding):
+            switch encoding {
+            case .queryString:
+                // parameter query
+                let queryItemArray = parameters.map {
+                    URLQueryItem(name: $0.key, value: $0.value as? String)
+                }
+                urlComponents.queryItems = queryItemArray
+                url = urlComponents.url
+            }
+        }
+        guard let url = url else {
+            throw MovieDownloadError.invalidURLString
+        }
+        
+        // method
+        var request = URLRequest(url: url)
+        request.httpMethod = target.method.rawValue
+        
+        // header
+        if let headerField = target.headers {
+            _ = headerField.map { (key, value) in
+                request.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+        return request
+    }
+}
