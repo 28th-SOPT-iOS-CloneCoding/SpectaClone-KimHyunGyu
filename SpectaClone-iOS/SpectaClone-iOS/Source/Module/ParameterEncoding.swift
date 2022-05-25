c7d480c8d63df09d526085cf84d143b8758c9176
hyun99999 <ypsophila97@icloud.com>
2022-05-26 02:24:42 +0900

    add: `ParameterEncoding` 추가
    - 파라미터를 인코딩 할때 사용할 구현부
    - 추후에 body 에 파라미터를 추가하는 것도 구현 예정

---
+++ SpectaClone-iOS/SpectaClone-iOS/Source/Module/ParameterEncoding.swift
@@ -0,0 +1,12 @@
+//
+//  ParameterEncoding.swift
+//  SpectaClone-iOS
+//
+//  Created by kimhyungyu on 2022/05/20.
+//
+
+import Foundation
+
+public enum ParameterEncoding {
+    case queryString
+}
