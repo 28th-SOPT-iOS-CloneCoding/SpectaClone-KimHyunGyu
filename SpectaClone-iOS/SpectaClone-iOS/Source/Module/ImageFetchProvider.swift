eac0465fd722204d7b80a1cc28ac0e719c9b9a29
hyun99999 <ypsophila97@icloud.com>
2022-05-30 23:29:10 +0900

    comment: 주석 오타 수정

--- SpectaClone-iOS/SpectaClone-iOS/Source/Module/ImageFetchProvider.swift
+++ SpectaClone-iOS/SpectaClone-iOS/Source/Module/ImageFetchProvider.swift
@@ -11,7 +11,7 @@ struct ImageFetchProvider {
     static let shared = ImageFetchProvider()
     private init() { }
     
-    /// URL 을 가지고 data 를 다운받아서 UIImage 로 변환하느 메서드.
+    /// URL 을 가지고 data 를 다운받아서 UIImage 로 변환하는 메서드.
     /// - Parameter urlString: URL 가 될 String 자료형의 값.
     /// - Returns: 다운 받은 data 를 UIImage 로 변환해서 리턴. 변환되지 않는 경우 에러를 던집니다.
     public func fetchImage(with urlString: String) async throws -> UIImage {
