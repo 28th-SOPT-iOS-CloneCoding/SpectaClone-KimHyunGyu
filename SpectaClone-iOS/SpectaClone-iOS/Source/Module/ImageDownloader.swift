324409759404bf78c231f90cfdeb406b74fdb89e
hyun99999 <ypsophila97@icloud.com>
2022-06-03 15:34:29 +0900

    feat: Actor 로 이미지를 캐싱하는 ImageDownloader 구현 (#7)

---
+++ SpectaClone-iOS/SpectaClone-iOS/Source/Module/ImageDownloader.swift
@@ -0,0 +1,44 @@
+//
+//  ImageDownloader.swift
+//  SpectaClone-iOS
+//
+//  Created by kimhyungyu on 2022/06/03.
+//
+
+import UIKit
+
+actor ImageDownloader {
+    static let shared = ImageDownloader()
+    private init() { }
+    
+    private var cache: [URL: UIImage] = [:]
+    
+    func image(from urlPath: String) async throws -> UIImage? {
+        guard let url = URL(string: Const.Path.imageURLPath + urlPath) else {
+            throw ImageDownloadError.invalidURLString
+        }
+        
+        if let cached = cache[url] {
+            return cached
+        }
+        
+        let image = try await downloadImage(from: url)
+        
+        cache[url] = cache[url, default: image]
+        
+        // FIXME: - 강제 추출...
+        return cache[url]!
+        
+        
+//        guard let cached = cache[url] else {
+//            throw ImageDownloadError.unsupportImage
+//        }
+//
+//        return cache[url]
+    }
+
+    private func downloadImage(from url: URL) async throws -> UIImage {
+        let imageFetchProvider = ImageFetchProvider.shared
+        return try await imageFetchProvider.fetchImage(with: url)
+    }
+}
